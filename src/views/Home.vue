7b6d3c2bc3a810e11a0d9d1918e55652743a2c11
Keelana Char <keelanachar@gmail.com>
2019-12-18 18:49:42 -0600

    authentication and index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="item in items">
+      <p>{{ item.name }}</p>
+      <p>{{ item.price }}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
@@ -8,13 +13,19 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      items: {}
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/items").then(response => {
+      this.items = (response.data);
+    });
+  },
   methods: {}
 };
 </script>

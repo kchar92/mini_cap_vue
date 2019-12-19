2211eba9c72e87542221ebe8dc552f4898d3bb28
Keelana Char <keelanachar@gmail.com>
2019-12-18 21:17:53 -0600

    add datalist- autocomplete

--- src/views/ItemsIndex.vue
+++ src/views/ItemsIndex.vue
@@ -2,7 +2,11 @@
   <div class="items-index">
     <h1>{{ message }}</h1>
 
-    Search: <input type="text" v-model="searchTerm">
+    Search: <input type="text" v-model="searchTerm" list="names">
+    <datalist id="names">
+      <option v-for="item in items"> {{ item.name }}</option>  
+    </datalist>
+    <br>
     <div v-for="item in filterBy(items, searchTerm, 'name')">
       <p>{{ item.name }}</p>
       <p>{{ item.price }}</p>
